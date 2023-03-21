424893e4e34b782ec10a8cee98a70f0de3ddbd29
guiac <gui.acassemiro@gmail.com>
2023-03-21 20:23:09 -0300

    :construction: env

--- src/main/config/env.ts
+++ src/main/config/env.ts
@@ -1,5 +1,9 @@
 export default {
   port: process.env.PORT || 3002,
   jwtSecret: process.env.JWT_SECRET || '1kZDnw8==jh',
-  mongoUrl: process.env.MONGO_URL || 'mongodb://127.0.0.1:27017/myapp'
+  mongoUrl: process.env.MONGO_URL || 'mongodb://127.0.0.1:27017/myapp',
+  kafkaClientId: process.env.KAFKA_CLIENTID || 'rem-kafka',
+  kafkaBrokerPort: process.env.KAFKA_BROKER_PORT || 9092,
+  kafkaBrokerHost: process.env.KAFKA_BROKER_HOST || 'broker',
+  kafkaGroupId: process.env.KAFKA_GROUP_ID || 'command-service'
 }
